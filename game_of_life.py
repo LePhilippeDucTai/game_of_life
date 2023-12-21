48d392d6e8dd45533a3830c05441809980d7a044
Philippe Lê <philippe.le@bpifrance.fr>
2023-12-21 17:01:04 +0100

    First commit : adding the first version of the game of life without pygame support.

---
+++ game_of_life.py
@@ -0,0 +1,23 @@
+import numpy as np
+import scipy.signal as sig
+
+KERNEL = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
+
+
+class GameOfLife:
+    def __init__(self, matrix: np.ndarray | None = None):
+        self.matrix = matrix
+
+    def generate_random(self, n, m, prob):
+        self.matrix = np.random.binomial(p=prob, n=1, size=(n, m))
+
+    def update(self):
+        conv = sig.convolve(self.matrix, KERNEL, mode="same")
+        rules = ((self.matrix == 1) & (conv == 2)) | (conv == 3)
+        self.matrix = np.where(rules, 1, 0)
+
+
+if __name__ == "__main__":
+    game = GameOfLife()
+    game.generate_random(50, 100, 0.3)
+    game.update()
